54f3fd23c59e049f03b2760e26531e6fa158ae8c
coderking3 <2196592083@qq.com>
2026-09-07 23:45:10 +0800

    fix(nitro.config): Fixes the CORS header configuration to ensure credentials are allowed.

--- nitro.config.ts
+++ nitro.config.ts
@@ -5,18 +5,22 @@ import { defineConfig } from 'nitro'
 export default defineConfig({
   serverDir: './server',
   preset: 'vercel',
+
   routeRules: {
     '/api/**': {
       cors: true,
       headers: {
         'Access-Control-Allow-Origin':
-          process.env.CORS_ORIGIN || 'http://localhost:3060'
+          process.env.CORS_ORIGIN || 'http://localhost:3060',
+        'Access-Control-Allow-Credentials': 'true'
       }
     }
   },
+
   runtimeConfig: {
     upstashRedisRestUrl: '',
     upstashRedisRestToken: ''
   },
+
   plugins: ['./server/plugins/logger.ts']
 })
